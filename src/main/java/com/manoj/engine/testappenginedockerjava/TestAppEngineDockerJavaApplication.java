0fb360055dc80b8645876b4b90c749766eeee783
manojbera <manoj.bera@vodafone.com>
2020-07-04 22:48:45 +0100

    First docker app engine commit

---
+++ src/main/java/com/manoj/engine/testappenginedockerjava/TestAppEngineDockerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.manoj.engine.testappenginedockerjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAppEngineDockerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAppEngineDockerJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manoj/engine/testappenginedockerjava/TestAppEngineDockerJavaApplicationTests.java
@@ -0,0 +1,34 @@
+package com.manoj.engine.testappenginedockerjava;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class TestAppEngineDockerJavaApplicationTests {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Test
+	public void testForJavaAppEngine() throws Exception {
+
+		MvcResult mvcResult=mockMvc.perform(get(
+			"/java").contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().is(200)).andReturn();
+		Assert.assertEquals("WellCome To App Engine With Docker", mvcResult.getResponse().getContentAsString() );
+	}
+
+}
